0bdcfeb97626580f2206d70ba963b206a1da16bf
Sanjeev Ganjihal <sanjeevganjihal@Sanjeevs-Air.attlocal.net>
2023-05-01 13:21:51 -0400

    code fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ChatApp from './components/ChatApp';
+
 
 ReactDOM.render(
   <React.StrictMode>
